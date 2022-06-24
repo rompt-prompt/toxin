2c1cdba3ac00e6c82acb240387da1434900e1a6f
rompt-promt <revstropov@gmail.com>
2022-06-24 09:13:14 +0300

    range slider added

---
+++ src/blocks/range-slider/_range-slider.js
@@ -0,0 +1,14 @@
+require('../../../node_modules/jquery-ui/ui/widgets/slider')
+
+$("#range-slider").slider({
+	animate: "slow",
+	range: true,
+	min: 0,
+	max: 17000,
+	values: [ 5000, 10000 ],
+	
+	slide : function(event, ui) {    
+		$("#range-legend").text(ui.values[0].toLocaleString() + "₽ - " + ui.values[ 1 ].toLocaleString() + "₽");        
+	}
+});
+$( "#range-legend" ).text($("#range-slider").slider("values", 0).toLocaleString() + "₽ - " + $("#range-slider").slider("values", 1).toLocaleString() + "₽");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './pages/_main.scss'
 import './blocks/dropdawn/_dropdawn.js'
 import './blocks/expandable/_expandable.js'
+import './blocks/range-slider/_range-slider.js'
