e8fc0f037f94f4f1b36000d1811439393842be09
rompt-promt <revstropov@gmail.com>
2022-07-06 06:23:12 +0300

    blocks modified

--- src/blocks/libs/datepicker/_datepicker.js
+++ src/blocks/libs/datepicker/_datepicker.js
@@ -10,6 +10,54 @@ let navTitle = {
     years: 'yyyy1 - yyyy2' 
 }
 
+new AirDatepicker('#searchDatepickerFrom', {
+    inline: true,
+    range: false,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'd MMM',
+    buttons: ['clear', apply],
+    navTitles: navTitle,
+    prevHtml: 'arrow_back',
+    nextHtml: 'arrow_forward',
+    toggleSelected: false,
+});
+
+new AirDatepicker('#searchDatepickerTo', {
+    inline: true,
+    range: false,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'd MMM',
+    buttons: ['clear', apply],
+    navTitles: navTitle,
+    prevHtml: 'arrow_back',
+    nextHtml: 'arrow_forward',
+    toggleSelected: false,
+});
+
+new AirDatepicker('#bookingDatepickerFrom', {
+    inline: true,
+    range: false,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'd MMM',
+    buttons: ['clear', apply],
+    navTitles: navTitle,
+    prevHtml: 'arrow_back',
+    nextHtml: 'arrow_forward',
+    toggleSelected: false,
+});
+
+new AirDatepicker('#bookingDatepickerTo', {
+    inline: true,
+    range: false,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'd MMM',
+    buttons: ['clear', apply],
+    navTitles: navTitle,
+    prevHtml: 'arrow_back',
+    nextHtml: 'arrow_forward',
+    toggleSelected: false,
+});
+
 new AirDatepicker('#datepicker', {
     inline: true,
     range: true,
--- src/blocks/libs/dropdawn/_dropdawn.js
+++ src/blocks/libs/dropdawn/_dropdawn.js
@@ -1,9 +1,19 @@
+var exception = [
+    'searchDateFrom',
+    'searchDateTo',
+    'bookingDateFrom',
+    'bookingDateTo',
+]
+
 show = function dropDawn() {
     
     document.onclick = function(event){
         var element = event.target.id;
-        console.log('Это ' + element);
-        document.getElementById(element).classList.toggle('dropdawn__btn_opened');
+        if (exception.includes(element)) {}
+        else {
+            document.getElementById(element).classList.toggle('dropdawn__btn_opened-border');
+        }
+        document.getElementById(element).classList.toggle('dropdawn__btn_opened-arrow');
         document.getElementById(element +'Content').classList.toggle('show');
     }
 }
