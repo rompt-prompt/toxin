7bc51d72ac389a082a3a3d3175b85cba9adda572
rompt-promt <revstropov@gmail.com>
2022-07-06 06:25:25 +0300

    form elements page modified

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,1 +1,5 @@
-import '../../index.js'
+import './form-elements.scss';
+import '../../blocks/libs/dropdawn/_dropdawn.js';
+import '../../blocks/libs/datepicker/_datepicker.js';
+import '../../blocks/libs/range-slider/_range-slider.js';
+// import '../../blocks/libs/expandable/_expandable.js'; // TODO expand logic
