a4c6699fd2c579a9fd2950bb565d40dbef8c1987
rompt-promt <revstropov@gmail.com>
2022-07-07 16:22:48 +0300

    cards page fixed

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,3 +1,4 @@
 import './cards.scss'
+import '../../blocks/libs/form-booking/_form-booking.js'
 import '../../blocks/libs/form-search/_form-search.js'
 import '../../blocks/libs/datepicker/_datepicker.js'
