0b688108a412197cea005b551da7f53f52004901
rompt-promt <revstropov@gmail.com>
2022-07-07 15:27:48 +0300

    search page added

---
+++ src/pages/search-filter/search-filter.js
@@ -0,0 +1,5 @@
+import './search-filter.scss';
+import '../../blocks/libs/dropdawn/_dropdawn.js';
+import '../../blocks/libs/datepicker/_datepicker.js';
+import '../../blocks/libs/range-slider/_range-slider.js';
+// import '../../blocks/libs/expandable/_expandable.js'; // TODO expand logic
